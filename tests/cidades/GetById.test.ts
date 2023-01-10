cf0db5ea1a6bba54b3495316c1b3d68ecc5f0b24
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2023-01-10 09:36:56 -0300

    fix: tests

--- tests/cidades/GetById.test.ts
+++ tests/cidades/GetById.test.ts
@@ -5,7 +5,7 @@ describe("Cidades - GetById", () => {
   it("Buscar registro por id", async () => {
     const resultCreate = await testServer
       .post("/cidades")
-      .send({ id: 1, nome: "Timon" });
+      .send({ nome: "Timon" });
 
     expect(resultCreate.statusCode).toEqual(StatusCodes.CREATED);
 
@@ -22,5 +22,26 @@ describe("Cidades - GetById", () => {
 
     expect(ressultSearch.statusCode).toEqual(StatusCodes.INTERNAL_SERVER_ERROR);
     expect(ressultSearch.body).toHaveProperty("errors.default");
+  });
+});
+
+describe("Cidades - GetById", () => {
+  it("Busca registro por id", async () => {
+    const res1 = await testServer
+      .post("/cidades")
+      .send({ nome: "Caxias do sul" });
+
+    expect(res1.statusCode).toEqual(StatusCodes.CREATED);
+
+    const resBuscada = await testServer.get(`/cidades/${res1.body}`).send();
+
+    expect(resBuscada.statusCode).toEqual(StatusCodes.OK);
+    expect(resBuscada.body).toHaveProperty("nome");
+  });
+  it("Tenta buscar registro que não existe", async () => {
+    const res1 = await testServer.get("/cidades/99999").send();
+
+    expect(res1.statusCode).toEqual(StatusCodes.INTERNAL_SERVER_ERROR);
+    expect(res1.body).toHaveProperty("errors.default");
   });
 });
--- tests/jest.setup.ts
+++ tests/jest.setup.ts
@@ -1,8 +1,8 @@
-import knex from "knex";
-import supertest from "supertest";
-import { Knex } from "../src/server/database/knex";
+import supertest from 'supertest';
+import { Knex } from '../src/server/database/knex';
 
-import { server } from "../src/server/Server";
+import { server } from '../src/server/Server';
+
 
 beforeAll(async () => {
   await Knex.migrate.latest();
@@ -12,5 +12,6 @@ beforeAll(async () => {
 afterAll(async () => {
   await Knex.destroy();
 });
+
 
 export const testServer = supertest(server);
