69784b80a835ceaa1b1389e40a9ee69aa03546a0
Gedeon Ramiro <gedeon_ramiro@hotmail.com>
2023-01-19 09:02:15 -0300

    feat: added authentication to pessoas and cidades endpoints

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -5,68 +5,80 @@ import {
   UsuariosController,
 } from "../controllers";
 
+import { endureAuthenticated } from "../shared/middleware/EndureAuthenticated";
+
 const router = Router();
 
-router.get("/", (_, res) => {
+router.get("/", endureAuthenticated, (_, res) => {
   return res.send("Olá, DEV!");
 });
 
 router.get(
   "/cidades",
+  endureAuthenticated,
   CidadesController.getAllValidation,
   CidadesController.getAll
 );
 
 router.get(
   "/cidades/:id",
+  endureAuthenticated,
   CidadesController.getByIdValidation,
   CidadesController.getById
 );
 
 router.post(
   "/cidades",
+  endureAuthenticated,
   CidadesController.createValidation,
   CidadesController.create
 );
 
 router.put(
   "/cidades/:id",
+  endureAuthenticated,
   CidadesController.updateByIdValidation,
   CidadesController.UpdateById
 );
 
 router.delete(
   "/cidades/:id",
+  endureAuthenticated,
   CidadesController.deleteByIdValidation,
   CidadesController.deleteById
 );
 
 router.get(
   "/pessoas",
+  endureAuthenticated,
   PessoasController.getAllValidation,
   PessoasController.getAll
 );
 
 router.get(
   "/pessoas/:id",
+  endureAuthenticated,
   PessoasController.getByIdValidation,
   PessoasController.getById
 );
 
 router.post(
   "/pessoas",
+  endureAuthenticated,
   PessoasController.createValidation,
   PessoasController.create
 );
 
 router.delete(
   "/pessoas/:id",
+  endureAuthenticated,
   PessoasController.deleteByIdValidation,
   PessoasController.deleteById
 );
 
 router.put(
   "/pessoas/:id",
+  endureAuthenticated,
   PessoasController.updateByIdValidation,
   PessoasController.UpdateById
 );
